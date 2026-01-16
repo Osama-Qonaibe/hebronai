f8efe14ef54f256603c991af542740f2470e99f0
Osama Qonaibe <qoomre@gmail.com>
2026-01-16 11:58:27 +0200

    Fix Stripe lazy initialization

--- lib/stripe/server.ts
+++ lib/stripe/server.ts
@@ -2,9 +2,26 @@ import Stripe from 'stripe'
 
 import { STRIPE_CONFIG } from './config'
 
-export const stripe = new Stripe(STRIPE_CONFIG.secretKey, {
-  apiVersion: STRIPE_CONFIG.apiVersion,
-  typescript: true
+let stripeInstance: Stripe | null = null
+
+function getStripe(): Stripe {
+  if (!stripeInstance) {
+    if (!STRIPE_CONFIG.secretKey) {
+      throw new Error('Stripe secret key not configured')
+    }
+    stripeInstance = new Stripe(STRIPE_CONFIG.secretKey, {
+      apiVersion: STRIPE_CONFIG.apiVersion,
+      typescript: true
+    })
+  }
+  return stripeInstance
+}
+
+export const stripe = new Proxy({} as Stripe, {
+  get: (target, prop) => {
+    const instance = getStripe()
+    return instance[prop as keyof Stripe]
+  }
 })
 
 export async function createCustomer(params: {
@@ -12,7 +29,7 @@ export async function createCustomer(params: {
   userId: string
   name?: string
 }): Promise<Stripe.Customer> {
-  return await stripe.customers.create({
+  return await getStripe().customers.create({
     email: params.email,
     name: params.name,
     metadata: {
@@ -27,7 +44,7 @@ export async function createCheckoutSession(params: {
   successUrl: string
   cancelUrl: string
 }): Promise<Stripe.Checkout.Session> {
-  return await stripe.checkout.sessions.create({
+  return await getStripe().checkout.sessions.create({
     customer: params.customerId,
     line_items: [
       {
@@ -46,7 +63,7 @@ export async function createBillingPortalSession(params: {
   customerId: string
   returnUrl: string
 }): Promise<Stripe.BillingPortal.Session> {
-  return await stripe.billingPortal.sessions.create({
+  return await getStripe().billingPortal.sessions.create({
     customer: params.customerId,
     return_url: params.returnUrl
   })
@@ -55,7 +72,7 @@ export async function createBillingPortalSession(params: {
 export async function cancelSubscription(
   subscriptionId: string
 ): Promise<Stripe.Subscription> {
-  return await stripe.subscriptions.update(subscriptionId, {
+  return await getStripe().subscriptions.update(subscriptionId, {
     cancel_at_period_end: true
   })
 }
@@ -63,7 +80,7 @@ export async function cancelSubscription(
 export async function reactivateSubscription(
   subscriptionId: string
 ): Promise<Stripe.Subscription> {
-  return await stripe.subscriptions.update(subscriptionId, {
+  return await getStripe().subscriptions.update(subscriptionId, {
     cancel_at_period_end: false
   })
 }
