a93458ddb6b8de33e27d5bcfe7b55a42e68958ed
Osama Qonaibe <qoomre@gmail.com>
2026-01-16 11:39:00 +0200

    Add Stripe server instance

---
+++ lib/stripe/server.ts
@@ -0,0 +1,68 @@
+import Stripe from 'stripe'
+import { STRIPE_CONFIG } from './config'
+
+export const stripe = new Stripe(STRIPE_CONFIG.secretKey, {
+  apiVersion: STRIPE_CONFIG.apiVersion,
+  typescript: true
+})
+
+export async function createCustomer(params: {
+  email: string
+  userId: string
+  name?: string
+}): Promise<Stripe.Customer> {
+  return await stripe.customers.create({
+    email: params.email,
+    name: params.name,
+    metadata: {
+      user_id: params.userId
+    }
+  })
+}
+
+export async function createCheckoutSession(params: {
+  customerId: string
+  priceId: string
+  successUrl: string
+  cancelUrl: string
+}): Promise<Stripe.Checkout.Session> {
+  return await stripe.checkout.sessions.create({
+    customer: params.customerId,
+    line_items: [
+      {
+        price: params.priceId,
+        quantity: 1
+      }
+    ],
+    mode: 'subscription',
+    success_url: params.successUrl,
+    cancel_url: params.cancelUrl,
+    allow_promotion_codes: true
+  })
+}
+
+export async function createBillingPortalSession(params: {
+  customerId: string
+  returnUrl: string
+}): Promise<Stripe.BillingPortal.Session> {
+  return await stripe.billingPortal.sessions.create({
+    customer: params.customerId,
+    return_url: params.returnUrl
+  })
+}
+
+export async function cancelSubscription(
+  subscriptionId: string
+): Promise<Stripe.Subscription> {
+  return await stripe.subscriptions.update(subscriptionId, {
+    cancel_at_period_end: true
+  })
+}
+
+export async function reactivateSubscription(
+  subscriptionId: string
+): Promise<Stripe.Subscription> {
+  return await stripe.subscriptions.update(subscriptionId, {
+    cancel_at_period_end: false
+  })
+}
