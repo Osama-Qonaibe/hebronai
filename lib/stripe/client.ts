62e8a09eff0990fe2dada432a59699791127de85
Osama Qonaibe <qoomre@gmail.com>
2026-01-16 11:39:11 +0200

    Add Stripe client utilities

---
+++ lib/stripe/client.ts
@@ -0,0 +1,11 @@
+import { loadStripe, Stripe } from '@stripe/stripe-js'
+import { STRIPE_CONFIG } from './config'
+
+let stripePromise: Promise<Stripe | null>
+
+export const getStripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(STRIPE_CONFIG.publishableKey)
+  }
+  return stripePromise
+}
